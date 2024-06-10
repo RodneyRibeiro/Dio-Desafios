13c35e3d9b6d5c6fe9a3ef7e07952e7c60da4e20
Rodney Ribeiro <136312023+RodneyRibeiro@users.noreply.github.com>
2024-06-10 16:47:11 +0100

    Correção de formatação.

--- Desafio_Supermercado.py
+++ Desafio_Supermercado.py
@@ -14,7 +14,7 @@ Faça um programa para calcular isso.
 
 Entrada
 A primeira linha de entrada contém inteiro T (1 ≤ T ≤ 10000) , que indica o número de casos de teste. Em cada uma das T linhas a seguir vêm dois inteiros N e K (1 ≤ K, N ≤ 10000),
-  respectivamente o número de refrigerantes comprados e o número de garrafas vazias para ganhar uma cheia.
+respectivamente o número de refrigerantes comprados e o número de garrafas vazias para ganhar uma cheia.
 
 Saída
 Para cada caso de teste imprima o número de garrafas que o cliente terá no segundo dia, se aproveitar ao máximo a oferta.
@@ -22,19 +22,18 @@ Para cada caso de teste imprima o número de garrafas que o cliente terá no seg
 T = int(input())
 
 for i in range(T):
+    i = input()
+    i = i.split()
+    
+    N = int(i[0])
+    K = int(i[1])   
 
-   i = input()
-   i = i.split()  
+    garrafas_cheias = N // K
+    garrafas_vazias = N % K   
 
-   N = int(i[0])
-   K = int(i[1])   
+    total = garrafas_vazias + garrafas_cheias
 
-   garrafas_cheias = N // K
-   garrafas_vazias = N % K   
-
-   total = garrafas_vazias + garrafas_cheias
-
-   print(total)
+    print(total)
 ''' 
 Ler as variáveis de entrada N e K. Talvez seja necessário fazer um "split" na linha 
     para obtenção dos valores.
