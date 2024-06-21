3b27f1245c24d16f02af8a6bfe08bd7d23c050c5
Rodney Ribeiro <136312023+RodneyRibeiro@users.noreply.github.com>
2024-06-21 21:31:38 +0100

    pacote de teste para desafio DIO

---
+++ pacote_teste/pacote_teste/module1/file2_name.py
@@ -0,0 +1,11 @@
+'''
+Modulo 2 para testes de pacotes
+'''
+
+def funcao2():
+    '''
+    Função básica para teste
+    '''
+    return "Hello from modulo2"
+
+# EOF
---
+++ pacote_teste/setup.py
@@ -0,0 +1,25 @@
+'''
+Setup
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name='pacote_teste',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],  # Lista de dependências
+    author='Rodney',
+    author_email='rodneyg.ribeiro@gmail.com',
+    description='Programa básico somente para teste',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RodneyRibeiro/Dio-Desafios/tree/Desafios_Dio/criacao_de_pacote_desafio_dio',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+#EOF
